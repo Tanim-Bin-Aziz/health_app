6f522043de2d3b48dfc0ddf21c741ed7772b5fc2
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-09-16 13:27:13 +0600

    login error fixed

--- src/services/actions/logoutUser.ts
+++ src/services/actions/logoutUser.ts
@@ -2,9 +2,14 @@ import { authKey } from "../../contants/authkey";
 import { deleteCookies } from "./deleteCookies";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-export const logoutUser = (router: AppRouterInstance) => {
+export const logoutUser = async (router: AppRouterInstance) => {
+  // Remove token from localStorage (client-side)
   localStorage.removeItem(authKey);
-  deleteCookies([authKey, "refreshToken"]);
+
+  // Delete cookies (server action)
+  await deleteCookies([authKey, "refreshToken"]);
+
+  // Redirect and refresh
   router.push("/");
   router.refresh();
 };
